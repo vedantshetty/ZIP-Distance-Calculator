e3cbdc74de5e62af371c98f7d7b2ce3e3fbe2a6a
Vedant <vshetty.vs@gmail.com>
2018-10-30 10:42:27 +0530

    Fixed init function

---
+++ test.py
@@ -0,0 +1,23 @@
+from Djikstra import *
+g = Graph()
+g.add_node('A')
+g.add_node('B')
+g.add_node('C')
+g.add_node('D')
+g.add_node('E')
+g.add_node('F')
+g.add_node('G')
+
+g.add_edge('A','B',12)
+g.add_edge('A','C',7)
+g.add_edge('B','D',1)
+g.add_edge('B','A',12)
+g.add_edge('D','E',8)
+g.add_edge('C','F',3)
+g.add_edge('D','G',5)
+g.add_edge('F','B',1)
+g.add_edge('F','G',2)
+g.add_edge('C','D',13)
+g.add_edge('E','B',6)
+
+print(dijkstra(g, 'A')['B'])
